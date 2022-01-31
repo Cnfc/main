8cc16fc6db696a87fe75221cbe667f4064c7bd72
Stanislav Dashkov <tmwcnfc@gmail.com>
2022-01-31 22:18:50 +0300

    add express-validator

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,6 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.get("/api/users/signin", (req, res) => {
+  const { email, password } = req.body;
+
   res.send("signin");
 });
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,12 @@ import express from "express";
 const router = express.Router();
 
 router.get("/api/users/signup", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || typeof email !== "string") {
+    res.status(400).send("Provide a valid email & Pass");
+  }
+  console.log(email, password);
   res.send("signup");
 });
 
