9e17615c1a7327e2ad023b065b0e3fb83024e4e1
Stanislav Dashkov <tmwcnfc@gmail.com>
2022-02-02 22:21:57 +0300

    restructure errors structure

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -2,11 +2,16 @@ import { ValidationError } from "express-validator";
 
 export class DatabaseConenctionError extends Error {
   reason = "Error connecting to Database";
+  statusCode = 500;
 
   constructor() {
     super();
 
     //
     Object.setPrototypeOf(this, DatabaseConenctionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -9,20 +9,16 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      return { message: error.msg, field: error.param };
-    });
-
-    return res.status(400).send({
-      errors: formattedErrors,
+    return res.status(err.statusCode).send({
+      errors: err.serializeError(),
     });
   }
 
   if (err instanceof DatabaseConenctionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: err.message }],
   });
 };
