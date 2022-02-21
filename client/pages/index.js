5d7f93f78785d96ed75e98a7c91a8cd97a137198
Stanislav Dashkov <tmwcnfc@gmail.com>
2022-02-21 18:48:46 +0300

    add css examples

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,21 @@
 import react from "react";
 
-export default () => {
+const LandingPage = () => {
   return <h2>Landing Page</h2>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // We are on the server
+    // const res = await axios.get("http://ingress-nginx-controller.ingress-nginx.srv.cluster.local/api/users/currentuser");
+  } else {
+    // We are on the browser
+    // const res = await axios.get("/api/users/currentuser");
+  }
+
+  console.log("I was Exex");
+
+  return {};
+};
+
+export default LandingPage;
