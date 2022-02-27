772a08a6003e37845c8fb7e4afc8437310d1a6d2
Stanislav Dashkov <tmwcnfc@gmail.com>
2022-02-27 17:11:31 +0300

    add Header & initialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,37 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps}></Component>;
+import axios from "axios";
+import buildClient from "../api/build-client";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header ! {currentUser.email}</h1>
+      <Component {...pageProps}></Component>;
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  console.log(Object.keys(appContext));
+
+  // const client = buildClient(context);
+
+  const { data } = await buildClient(appContext.ctx).get(
+    "/api/users/currentuser"
+  );
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps, "PagePROPS");
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
