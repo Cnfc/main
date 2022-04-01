693bc5e668bd6a8b6d9075f550e54f9c1390bd83
Stanislav Dashkov <tmwcnfc@gmail.com>
2022-04-01 22:34:27 +0300

    add auto text typing animation

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
+// import '../styles/globals.css'
+
 import buildClient from "../api/build-client";
 import Header from "../components/header";
 
---
+++ client/pages/home/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./styles.module.css";
+
+const Home = (props) => {
+  return (
+    <div>
+      <div className={styles.title}>Pure Css Animation Section</div>
+    </div>
+  );
+};
+
+export default Home;
